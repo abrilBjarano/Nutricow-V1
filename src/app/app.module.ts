cbf8d0fc9fccb43c4010a2f70779d959c9468ebe
abrilxcx <abrlbjrn@gmail.com>
2024-02-17 18:12:28 -0600

    Categories centered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { NutricowModule } from './nutricow/nutricow.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    NutricowModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
